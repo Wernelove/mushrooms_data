a6833c34d35b005d4361c91df20a2e57512edb53
WernerLove <73615240+Wernelove@users.noreply.github.com>
2023-10-06 12:15:24 -0300

    Update get_urls.py

--- get_urls.py
+++ get_urls.py
@@ -15,22 +15,27 @@ def find_links_from_url(url):
         print(f"Erro ao acessar {url}: {str(e)}")
         return []
 
+def get_urls(start_url, links_list):
+  # Encontre todos os links a partir do URL de partida
+  links = find_links_from_url(start_url)
+  
+  # Palavras-chave para procurar nos links
+  keywords = ['edible', 'inedible', 'poisonous']
+  
+  # Use list comprehension para criar uma nova lista apenas com os links relevantes
+  links = [link for link in links if any(keyword in link for keyword in keywords)]
+  
+  del links[:3]
+  del links[-3:]
+      
+  # Adicione os links relevantes à lista fornecida como argumento
+  links_list.extend(links)
+
 # URL de partida
 start_url = 'https://ultimate-mushroom.com/mushroom-alphabet.html'
+result_links = []  # Crie uma lista vazia para armazenar os links relevantes
+get_urls(start_url, result_links)
 
-# Encontre todos os links a partir do URL de partida
-links = find_links_from_url(start_url)
-
-# Palavras-chave para procurar nos links
-keywords = ['edible', 'inedible', 'poisonous']
-
-# Use list comprehension para criar uma nova lista apenas com os links relevantes
-links = [link for link in links if any(keyword in link for keyword in keywords)]
-
-del links[:3]
-del links[-3:]
-
-# Imprima os links relevantes
-for link in links:
+# Agora, result_links contém os links relevantes
+for link in result_links:
     print(link)
-
